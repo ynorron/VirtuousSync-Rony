1fdf4608ff99208d280bdc483924a4bd20d4eb8a
rnunez <rony.nunez@kraferd.com>
2025-01-16 12:59:32 -0500

    Adding conditions folder

---
+++ Sync/Conditions/Condition.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Sync
+{
+    public class Condition
+        {
+            public string Parameter { get; set; }
+            public string Operator { get; set; }
+            public string Value { get; set; }
+            public string SecondaryValue { get; set; }
+            public List<string> Values { get; set; }
+        }
+
+}
